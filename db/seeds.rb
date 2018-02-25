06023570d1ef28685ee7d7b5c29d54ce8826ab5b
gilsunshine <gilsunshine@gmail.com>
2018-02-25 12:38:34 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'Gil', last_name: 'Sunshine')
+Student.create(first_name: 'Reuven', last_name: 'Sunshine')
